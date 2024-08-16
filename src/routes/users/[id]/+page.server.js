94e4519445491c2e6ffb58d2bd6ea13b8f57f3a3
Genevieve CLoutier <shail1234@hotmail.com>
2024-08-16 16:54:12 -0400

    ajout page erreur serveur

--- src/routes/users/[id]/+page.server.js
+++ src/routes/users/[id]/+page.server.js
@@ -1,8 +1,15 @@
 import { findOne } from "../../../lib/db/controllers/users.controller";
+import { error } from '@sveltejs/kit';
 
 export async function load({ params }){
     const idUser = params.id;
     const user = await findOne({ id: idUser });
+
+    if (!idUser) {
+		error(404, {
+			message: 'Not found'
+		})
+    } 
 
     return { user:user };
 }
