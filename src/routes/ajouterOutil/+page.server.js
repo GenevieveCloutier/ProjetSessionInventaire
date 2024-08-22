b8cc1b276fad07921fd27738613c86d9bb814647
paolabhering <156713438+paolabhering@users.noreply.github.com>
2024-08-22 18:25:52 -0400

    Create +page.server.js

---
+++ src/routes/ajouterOutil/+page.server.js
@@ -0,0 +1,32 @@
+import { newItem } from "$lib/db/controllers/items.controller"; 
+
+
+export const actions = {
+    default: async ({ request }) => {
+        try {
+            const formData = await request.formData();
+
+            const nom = formData.get('nom');
+            const description = formData.get('description');
+            const categorie = formData.get('categorie');
+            const quantite = formData.get('quantite');
+            //const image = formData.get('image'); 
+
+            console.log("Form Data Received:", { nom, description, categorie, quantite });
+
+            if (!nom || !description || !categorie || !quantite) {
+                console.error("Missing required fields");
+                return { success: false, error: "Missing required fields" };
+            }
+
+            const result = await newItem(nom, description, categorie, quantite, 'Disponible');
+
+            console.log("New item added successfully:", result);
+            return { success: true };
+
+        } catch (error) {
+            console.error("Error while creating a new item:", error);
+            return { success: false, error: error.message };
+        }
+    }
+};
