f2b82e4ad0e0964935b5e944d92b1df476ab0f25
Genevieve CLoutier <shail1234@hotmail.com>
2024-08-23 10:10:19 -0400

    ajustement items.controller

--- src/routes/items/+page.server.js
+++ src/routes/items/+page.server.js
@@ -1,7 +1,7 @@
 
 import { findAll } from "$lib/db/controllers/items.controller";
 
-export async function load({params, url}) {
+export async function load({params}) {
     const items = await findAll();
     return { items:items};
 }
