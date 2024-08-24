5af62df9e8d16eb5df0794ddbce09eacfaca7b36
Genevieve CLoutier <shail1234@hotmail.com>
2024-08-24 09:43:55 -0400

    boite de recherche

--- src/routes/items/+page.server.js
+++ src/routes/items/+page.server.js
@@ -1,7 +1,17 @@
 
-import { findAll } from "$lib/db/controllers/items.controller";
+// import { findAll } from "$lib/db/controllers/items.controller";
 
-export async function load({params}) {
+// export async function load({params}) {
+//     const items = await findAll();
+//     return { items:items};
+// }
+
+import { findAll } from "$lib/db/controllers/items.controller";
+import { rechercher } from "$lib/db/controllers/items.controller";
+
+export async function load({params, url}) {
     const items = await findAll();
-    return { items:items};
-}
+    let resultat = url.searchParams.get("resultatRecherche");
+    const itemRecherche = await rechercher(resultat);
+    return { items:items, itemRecherche:itemRecherche};  
+};
--- src/routes/items/+page.svelte
+++ src/routes/items/+page.svelte
@@ -7,15 +7,35 @@
 
     export let data;
     const items = data.items;
+    const recherche = data.itemRecherche;
+    console.log(recherche)
 
-    console.log(items)
+    //console.log(items)
 
 </script>
 
 <Entete/>
 <Recherche />
 
-<div class="grid-container">
+{#each recherche as r}
+<h1>Résultats de recherche</h1>
+<div class="grid-container" id="outilRecherche">
+    <div class="grid-item">
+    <ul>
+        <img src="src/images/{r.image}" width="200" height="230" alt={r.nom}>
+        <h2>{r.nom}</h2>
+        <p>qty disponible: {r.quantite}</p>
+        <p>date de disponibilité: {r.description}</p>
+        <h3>#{r.id}</h3>
+        <BoutonBrun lien={`items/${r.id}`}  texte={"Louer"} />
+    </ul>
+    </div>
+</div>
+{/each}
+
+
+<h1 id="titreTous">Tous les outils</h1>
+<div class= "grid-container" id="tousOutils">
     {#each items as item}
         <div class="grid-item">
             <ul>
@@ -31,6 +51,4 @@
         </div>
     {/each}
 </div>
-
-
 
